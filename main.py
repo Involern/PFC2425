1f3675f45551ab9f1e242783c186d008952167e7
Involern <abdulah.alrifai@outlook.com>
2024-08-21 18:26:45 -0600

    Variables

--- main.py
+++ main.py
@@ -1,3 +1,68 @@
-# Printing
-print("Burgers")
-print("It's really good")
+fav_food = "burgers"
+fav_food_2 = 'pizza'
+email = 'fakeemail@fake.com'
+
+
+## Strings
+# print(fav_food)
+print(f"I like {fav_food} and {fav_food_2}")
+print(f"Your email is {email}")
+
+#Integers
+age = 24
+quantity = 3
+num_of_students = 30
+
+print(f"Player's age is {age}")
+print(f"You are purchasing {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is ${price}")
+print(f"Your gpa is: {gpa}")
+print(f"You ran {distance}km")
+
+# Boolean
+
+is_student = False
+for_sale = False
+is_online = True
+
+print(f"Student Status: {is_student}")
+
+if is_student:
+    print("You are a student")
+else:
+    print('You are not a student')
+
+if for_sale:
+    print("That item is for sale")
+else:
+    print("That item is NOT available")
+
+if is_online:
+    print("You are online")
+else:
+    print("You are offline")
+
+
+Username = 'WKS'
+onlinestat = True
+playtime = 5.6
+age = 27
+
+print(f'''Username: {Username}.
+          Online Status: {onlinestat}.
+          Play Time: {playtime}hrs.
+          Age: {age}''')
+
+
+
+
+
+
+
