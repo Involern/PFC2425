39db638caa11f7906add7e62cb3a34c0e6e32006
Involern <abdulah.alrifai@outlook.com>
2024-08-24 16:41:46 -0600

    User input

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import time
-t = time
+# length = float(input("Enter a length: "))
+# width = float(input("Enter a width: "))
+# area = length * width
 
-while True:
-    USER = input("Please enter your Username: ")
-    if bool(USER):
-        print(f"Great! Your name is {USER}")
-        break
-    else:
-        print("Invalid")
-        t.sleep(0.5)
+# print(f"The area is: {area}cm²")
+
+# Shopping Cart Program
+
+item = input("What item would you like to buy?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
+total = price * quantity
+
+print(f"You have bought {quantity} x {item}/s")
+print(f"Your total is: ${total}")
