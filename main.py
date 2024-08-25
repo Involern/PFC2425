d0f2a171cf4d38af30bd1d39659325cb2ce04d09
Involern <abdulah.alrifai@outlook.com>
2024-08-24 21:08:50 -0600

    Conditional expression. One lined if statement

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-# logical operators in Python
+# conditional expression
 
-temp = 20
-is_sunny = False
 
-if temp >= 28 and is_sunny:
-    print("It is HOT outside")
-    print("It is SUNNY")
-elif temp <= 0 and is_sunny:
-    print("It is COLD outside")
-    print("It is SUNNY")
-elif 28 > temp > 0 and is_sunny:
-    print("It is WARM outside")
-    print("It is SUNNY")
-elif temp >= 28 and not is_sunny:
-    print("It is HOT outside")
-    print("It is CLOUDY")
-elif temp <= 0 and not is_sunny:
-    print("It is COLD outside")
-    print("It is CLOUDY")
-elif 28 > temp > 0 and not is_sunny:
-    print("It is WARM outside")
-    print("It is CLOUDY")
+num = 6
+a = 6
+b = 7
+age = 13
+temperature = 20
+user_role = "guest"
+
+# print("Positive" if num > 0 else "Negative")
+# result = "EVEN" if num % 2 == 0 else "ODD"
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+# status = "Adult" if age >= 18 else "Child"
+# weather = "HOT" if temperature > 20 else "COLD"
+access_level = "Full Access" if user_role == "admin" else "Limited Access"
+
+print(access_level)
