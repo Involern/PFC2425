5bc0ab3d7a4f1ebf9408cfa30489e11c003591ca
Involern <abdulah.alrifai@outlook.com>
2024-08-24 18:52:18 -0600

    If statements

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
+online = False
 
-a = float(input("Enter side A: "))
-b = float(input("Enter side B: "))
-
-c = math.sqrt(pow(a, 2) + pow(b, 2))
-
-print(f"Side C = {c}")
+if online:
+    print("The user is online")
+else:
+    print("The user is offline")
