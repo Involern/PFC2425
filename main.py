8daa3826fd37a391017175856dd4ea6a6af1d8ef
Involern <abdulah.alrifai@outlook.com>
2024-08-24 21:25:02 -0600

    string method proj

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-# String methods
+username = input("Enter a username: ")
 
-# name = input("Enter your full name: ")
-phone_number = input("Enter your phone number: ")
 
-# result = len(name)
-# result = name.find("B")
-# result = name.rfind("q")
-# name = name.capitalize()
-# name = name.upper()
-# name = name.lower()
-# result = name.isdigit()
-# result = name.isalpha()
-# result = phone_number.count("-")
-phone_number = phone_number.replace("-", "")
-
-print(phone_number)
-
-print(help(str))
+if len(username) > 12:
+    print("Your username can't be more than 12 characters.")
+elif not username.find(" ") == -1:
+    print("Your username can't contain spaces")
+elif not username.isalpha():
+    print("Your username can't contain numbers")
+else:
+    print(f"Welcome {username}")
