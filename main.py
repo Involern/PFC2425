c2b03761a9dfac05479aad9f20ee7d48fbfbcc5c
Involern <abdulah.alrifai@outlook.com>
2024-08-24 18:12:36 -0600

    Madlibs game

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-# length = float(input("Enter a length: "))
-# width = float(input("Enter a width: "))
-# area = length * width
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing': ")
+adjective3 = input("Enter an adjective (description): ")
 
-# print(f"The area is: {area}cm²")
 
-# Shopping Cart Program
 
-item = input("What item would you like to buy?: ")
-price = float(input("What is the price?: "))
-quantity = int(input("How many would you like?: "))
-total = price * quantity
-
-print(f"You have bought {quantity} x {item}/s")
-print(f"Your total is: ${total}")
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
