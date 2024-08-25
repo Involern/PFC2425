0d3fdb4ab78b1646f0699106eb23f89b11574289
Involern <abdulah.alrifai@outlook.com>
2024-08-24 21:20:20 -0600

    string methods

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-# conditional expression
+# String methods
 
+# name = input("Enter your full name: ")
+phone_number = input("Enter your phone number: ")
 
-num = 6
-a = 6
-b = 7
-age = 13
-temperature = 20
-user_role = "guest"
+# result = len(name)
+# result = name.find("B")
+# result = name.rfind("q")
+# name = name.capitalize()
+# name = name.upper()
+# name = name.lower()
+# result = name.isdigit()
+# result = name.isalpha()
+# result = phone_number.count("-")
+phone_number = phone_number.replace("-", "")
 
-# print("Positive" if num > 0 else "Negative")
-# result = "EVEN" if num % 2 == 0 else "ODD"
-# max_num = a if a > b else b
-# min_num = a if a < b else b
-# status = "Adult" if age >= 18 else "Child"
-# weather = "HOT" if temperature > 20 else "COLD"
-access_level = "Full Access" if user_role == "admin" else "Limited Access"
+print(phone_number)
 
-print(access_level)
+print(help(str))
