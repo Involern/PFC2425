ba2fa98b55a39a3b9dfb22ea4bd1d26f1cb629b1
Involern <abdulah.alrifai@outlook.com>
2024-08-24 21:33:36 -0600

    string indexing

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-username = input("Enter a username: ")
+# indexing
 
+credit_number = "1234-5812-2112-3211"
 
-if len(username) > 12:
-    print("Your username can't be more than 12 characters.")
-elif not username.find(" ") == -1:
-    print("Your username can't contain spaces")
-elif not username.isalpha():
-    print("Your username can't contain numbers")
-else:
-    print(f"Welcome {username}")
+# print(credit_number[4])
+# print(credit_number[:4])
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[-4])
+# print(credit_number[::2])
+# last_digits = credit_number[-4:]
+
+credit_number = credit_number[::-1]
+print(credit_number)
+# print(f"XXXX-XXXX-XXXX-{last_digits}")
