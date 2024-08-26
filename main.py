4b06c0cdd88c6e6719d4895fb1a1b4313ab24e0d
Involern <abdulah.alrifai@outlook.com>
2024-08-25 19:50:58 -0600

    Countdown timer program

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-# for loops
+import time
 
-for x in range(1, 21):
-    if x == 13:
-        break
-    else:
-        print(x)
+my_time = int(input("Enter the time in seconds: "))
+
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("TIME'S UP!")
