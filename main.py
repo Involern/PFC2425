6a2c8faba5f7babbea8bf880131fb1780a2bc43b
Involern <abdulah.alrifai@outlook.com>
2024-08-26 16:19:04 -0600

    Experimented with .dat file reading.

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-# nested loop
+file_path = 'save.dat'
 
-rows = int(input("Enter the # of rows: "))
-columns = int(input("Enter the # of columns: "))
-symbol = input("Enter a symnol to use: ")
+with open(file_path, 'r') as file:
+    data = file.readlines()
 
+data = [line.strip() for line in data]
 
-for x in range (rows):
-    for y in range(columns):
-        print(symbol, end="")
-    print()
+print(data[4])
