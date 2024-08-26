da2c76cc645053f71876f23eae1fb4c0dae50993
Involern <abdulah.alrifai@outlook.com>
2024-08-25 19:57:06 -0600

    nested loops

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import time
+# nested loop
 
-my_time = int(input("Enter the time in seconds: "))
+rows = int(input("Enter the # of rows: "))
+columns = int(input("Enter the # of columns: "))
+symbol = input("Enter a symnol to use: ")
 
-for x in range(my_time, 0, -1):
-    seconds = x % 60
-    minutes = int(x / 60) % 60
-    hours = int(x / 3600)
-    print(f"{hours:02}:{minutes:02}:{seconds:02}")
-    time.sleep(1)
 
-print("TIME'S UP!")
+for x in range (rows):
+    for y in range(columns):
+        print(symbol, end="")
+    print()
