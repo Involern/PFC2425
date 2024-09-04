2c7e06b03e6c24ebda94a6ab0161b879648e09cc
Involern <abdulah.alrifai@outlook.com>
2024-09-04 10:05:51 -0600

    list methods

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-# collection
+# Collection
 
-fruits = ["apple", "orange", "banana", "coconut"]
+fruits = ['apple', 'orange', 'banana', 'coconut']
 
+# print(fruit[::-1])
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("Pineapple" in fruits)
 
-for x in 
-print(fruits[4])
+# fruits[1] = 'pineapple'
+# fruits.append('pineapple')
+# fruits.remove('apple')
+# fruits.insert(0, "pineapple")
+# fruits.sort() # sorts in alphabetical order
+# fruits.reverse() #reversed based on order they are placed.
+# fruits.reverse()
+# fruits.clear()
+# print(fruits.index("pineapple"))
+# print(fruits.count('pineapple'))
+
+print(fruits)
+# for fruit in fruits:
+#     print(fruit)
