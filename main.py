f444b0d2238bfe3af6bb724a1c7a4e6367c3a55e
Involern <abdulah.alrifai@outlook.com>
2024-09-10 08:33:08 -0600

    New stage changes

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
-# Collection
-# Set: Unordered and immutable, but Add/Remove OK. NO duplicates unlike lists.
+# # Collection
+# # Tuples. Ordered and unchangeable. Duplicates OK. FASTER
 
 
-fruits = {'apple', 'orange', 'banana', 'coconut', 'coconut'}
-
-# print(dir(fruits))
+# fruits = ('apple', 'orange', 'banana', 'coconut', 'coconut')
+# # print(dir(fruits))
 # print(help(fruits))
-# print(len(fruits))
-# print('pineapples' in fruits)
-# print(fruits[0])
-# fruits.add('pineapple')
-# fruits.remove('apple')
-# fruits.pop()
-# fruits.clear()
 
-print(fruits)
+# print(fruits)
+
+
+multiplier_map = {
+    -6: .25,
+    -5: .29,
+    -4: .33,
+    -3: .4,
+    -2: .5,
+    -1: .67,
+    0: 0,
+    1: 1.5,
+    2: 2,
+    3: 2.5,
+    4: 3,
+    5: 3.5,
+    6: 4
+}
+
+base = 2
+stage = 0
+mult = multiplier_map.get(stage, -6 if stage <= -6 else (6 if stage >= 6 else stage))
+
+print('Too high or low stage!' if mult >= -6 or mult <= 6 else '')
+
+def multiplierBase(baset):
+    newVal = baset * (1 if stage == 0 else mult)
+    print(f"base is {baset} and multiplier is {mult}")
+    if stage -5 or -4 or -1:
+        round(newVal, 2)
+    return newVal
+
+print(f'Final stat {multiplierBase(base)}')
