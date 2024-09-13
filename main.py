64360f6d40df7794b3fe31fdcb9a2f8c28ffde4a
Involern <abdulah.alrifai@outlook.com>
2024-09-12 20:39:04 -0600

    Collection

--- main.py
+++ main.py
@@ -1,46 +1,25 @@
-import time as t
+# collection = single "variable" used to store multiple values
+# list: order and changeable. Duplicate ok
+# Set: unordered and immutable, but Add/Remvoe OK. No duplicates
+# Tuple: () ordered and unchangeable. Duplicates ok. Faster
 
-# # Collection
-# # Tuples. Ordered and unchangeable. Duplicates OK. FASTER
+fruits = ("apple", "orange", "banana", "coconut", "apple", "apple")
 
-
-# fruits = ('apple', 'orange', 'banana', 'coconut', 'coconut')
-# # print(dir(fruits))
+# print(dir(fruits))
 # print(help(fruits))
+# print(len(fruits))
+# print("pineapple" in fruits)
 
-# print(fruits)
+# print(fruits[0]) # cannot use indexing on set because it is unordered
 
+# fruits.add("pineapple")
+# fruits.remove("apple")
+# fruits.pop()
+# fruits.clear()
+# print(fruits.index("apple"))
+# print(fruits.count("apple"))
 
-multiplier_map = {
-    -6: .25,
-    -5: .29,
-    -4: .33,
-    -3: .4,
-    -2: .5,
-    -1: .67,
-    0: 0,
-    1: 1.5,
-    2: 2,
-    3: 2.5,
-    4: 3,
-    5: 3.5,
-    6: 4
-}
+for fruit in fruits:
+    print(fruit)
 
-base = 52
-stage = 8
-mult = multiplier_map.get(stage, -6 if stage <= -6 else (6 if stage >= 6 else stage))
-
-# print('Too high or low stage!' if mult >= -6 or mult <= 6 else '')
-
-def multiplierBase(baset):
-    newVal = baset * (1 if stage == 0 else mult)
-    print(f"base is {baset} and multiplier is {mult}")
-    if stage in [-5, -4, -1]:
-        round(newVal, 2)
-    return newVal
-
-print(f'Final stat {multiplierBase(base)}')
-
-t.sleep(2)
-print('slept!')
+print(fruits)
