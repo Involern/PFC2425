14ea3672d315cdc90a4c7a6e8c9ceac7b7de0c87
Involern <abdulah.alrifai@outlook.com>
2024-10-18 21:23:00 -0600

    keyword arguments

--- main.py
+++ main.py
@@ -2,3 +2,22 @@
 #                     helps with readability
 #                     order of arguments doesn't matter
 #                     1. Positional 2. Default 3. KEYWORD 4. Arbitrary
+
+def get_phone(country, area, first, last):
+    return f"{country}-{area}-{first}-{last}"
+
+phone_num = get_phone(country=1, area=123, first=456, last=7890)
+print(phone_num)
+
+# print("1", "2", "3", "4", "5", sep="-") #keyword argument is "sep"
+
+
+
+# def hello(greeting, title, first, last):
+#     print(f"{greeting} {title}{first} {last}")
+
+# hello(first="Spongebob", greeting="Hello", last="Squarepants", title="Mr.") #doesn't matter position of the arugments.
+
+
+# for x in range(1, 11):
+#     print(x, end=" ") #end is a keyword argument in print.
