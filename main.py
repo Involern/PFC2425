d074c1aeb58c8a053240de81f58136d5da06e157
Involern <abdulah.alrifai@outlook.com>
2024-10-19 20:26:02 -0600

    list comprehension

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # List comprehension = A concise way to create lists in Python
 #                      Compact and easier to read than traditional loops
 #                      [expression for value in iterable if condition]
+# LIST DONE
 
 grades = [85, 42, 79, 90, 56, 61, 30]
 passing_grades = [grade for grade in grades if grade >= 60]
