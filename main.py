0374972a3db4331886a3a81c7f30cf90bcf33c57
Involern <abdulah.alrifai@outlook.com>
2024-10-19 20:49:53 -0600

    match-case statements

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
-# List comprehension = A concise way to create lists in Python
-#                      Compact and easier to read than traditional loops
-#                      [expression for value in iterable if condition]
-# LIST DONE
+# Match-case statement (switch): An alternative to using many 'elif' statements
+#                                Execute some code if a value matches a 'case'
+#                                Benefits: cleaner and syntax is more readable
 
-grades = [85, 42, 79, 90, 56, 61, 30]
-passing_grades = [grade for grade in grades if grade >= 60]
+#match-case variant:
 
-print(passing_grades)
+def is_weeekend(day):
+    match day:
+        case "Saturday" | "Sunday":
+            return True
+        case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday":
+            return False
+        case _: #instead if "else", use case _ which is the wild card.
+            return False
 
-
-# numbers = [-1, -2, 3, -4, 5, -6, 8, -7]
-# positive_nums = [num for num in numbers if num >= 0] # returns num if number is less than 0
-# negative_nums = [num for num in numbers if num < 0]
-# even_nums = [num for num in numbers if num % 2 == 0]
-# odd_nums = [num for num in numbers if num % 2 == 1]
-
-# print(odd_nums)
+print(is_weeekend("Pizza"))
 
 
-# fruits = [fruit.upper() for fruit in ["apple", "orange", "banana", "coconut"]]
-# fruit_chars = [fruit[0] for fruit in fruits]
-# print(fruit_chars)
-
-# doubles = [x * 2 for x in range(1, 11)]
-# triples = [y * 3 for y in range(1, 11)]
-# squares = [z * z for z in range(1, 11)]
-
-# print(squares)
-
-# doubles = []
-# for x in range(1, 11):
-#     doubles.append(x * 2)
-
-# print(doubles)
+# def day_of_week(day):
+#     if day == 1:
+#         return "It is Sunday"
+#     elif day == 2:
+#         return "It is Monday"
+#     elif day == 3:
+#         return "It is Tuesday"
+#     elif day == 4:
+#         return "It is Wednesday"
+#     elif day == 5:
+#         return "It is Thursday"
+#     elif day == 6:
+#         return "It is Friday"
+#     elif day == 7:
+#         return "It is Saturday"
+#     else:
+#         return "Not a valid day"
+    
+# print(day_of_week(1))
