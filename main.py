e77b66cad77e7c55e3006f2b814feae0d2155c6d
Involern <abdulah.alrifai@outlook.com>
2024-11-01 09:37:12 -0600

    if __name__

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-# variable scope = where a variable is visible and accesible
-# scope resolution = (LEGB) Local -> Enclosed -> Global -> Built-in
-
-from math import e # built in version of e via math module
-
-def func1():
-    print(e)
-
-# e = 3 # global version of e
-
-func1()
-
-# def func1():
-#     print(x)
-
-# def func2():
-#     print(x)
-
-# x = 3
-
-# func1()
-# func2()
+# if __name__ == __main__: (this script can be imprted OR run standalone)
+#                          Functions and classes in this module can be reused
+#                          without the main block of code executing
